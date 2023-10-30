9b7539127773090a8eebd214f2e3478756d56c7e
kwazzar <fugitiwe@gmail.com>
2023-10-30 20:20:30 +0200

    Itunes API parsing

---
+++ DriveMusic/SearchResponse.swift
@@ -0,0 +1,20 @@
+//
+//  SearchResponse.swift
+//  DriveMusic
+//
+//  Created by Quasar on 28.10.2023.
+//
+
+import Foundation
+
+struct SearchResponse: Codable {
+    let resultCount: Int
+    let results: [Track]
+}
+
+struct Track: Codable {
+    let trackName: String
+    let collectionName: String?
+    let artistName: String
+    let artworkUrl100: String?
+}
--- DriveMusic/SearchViewController.swift
+++ DriveMusic/SearchViewController.swift
@@ -13,28 +13,27 @@ struct TrackModel {
     var artistName: String
 }
 
-
 class SearchViewController: UITableViewController {
+
+    private var timer: Timer?
     
-    let seachController = UISearchController(searchResultsController: nil)
-    let tracks = [TrackModel(trackName: "Wasted", artistName: "Metz"),
-                 TrackModel(trackName: "Wet Blanket", artistName: "Metz")]
-    
+    let searchController = UISearchController(searchResultsController: nil)
+    var tracks = [Track]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .white
         setupSearchBar()
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
-    
     }
     
     private func setupSearchBar() {
-        navigationItem.searchController = seachController
+        navigationItem.searchController = searchController
         navigationItem.hidesSearchBarWhenScrolling = false
-        seachController.searchBar.delegate = self
+        searchController.searchBar.delegate = self
     }
-    
+
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return tracks.count
     }
@@ -44,6 +43,8 @@ class SearchViewController: UITableViewController {
         cell.textLabel?.text = "\(track.trackName)\n\(track.artistName)"
         cell.textLabel?.numberOfLines = 2
         cell.imageView?.image = UIImage(imageLiteralResourceName: "Image")
+
+
         return cell
     }
 }
@@ -52,17 +53,32 @@ extension SearchViewController: UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         print(searchText)
 
-        let urlAF = "https://itunes.apple.com/search?term=\(searchText)"
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false, block: { (_) in
+            let urlAF = "https://itunes.apple.com/search?"
+            let parameters = ["term":"\(searchText)", "limit":"10"]
 
-        AF.request(urlAF).responseData { (dataResponse) in
-            if let error = dataResponse.error {
-                print("Error received requestiong data: \(error.localizedDescription)")
-                return
+            AF.request(urlAF, method: .get, parameters: parameters, encoding: URLEncoding.default).responseData {  (dataResponse) in
+                if let error = dataResponse.error {
+                    print("Error received requestiong data: \(error.localizedDescription)")
+                    return
+                }
+
+                guard let data = dataResponse.data else { return }
+
+                let decoder = JSONDecoder()
+                do {
+                    let objects = try decoder.decode(SearchResponse.self, from: data)
+                    print("objects: ", objects)
+                    self.tracks = objects.results
+                    self.tableView.reloadData()
+                } catch let jsonError {
+                    print("Failed to decode JSON", jsonError)
+
+                }
+//                let someString = String(data: data, encoding: .utf8)
+//                print(someString ?? "")
             }
-
-            guard let data = dataResponse.data else { return }
-            let someString = String(data: data, encoding: .utf8)
-            print(someString ?? "")
-        }
+        })
     }
 }
