222052647fb5a19de2e49f151a77659d9a8e653c
kwazzar <fugitiwe@gmail.com>
2023-11-10 01:04:11 +0200

    Scale animation and Time Observer in TrackDetailView

--- DriveMusic/Search/TrackDetailView/TrackDetailView.swift
+++ DriveMusic/Search/TrackDetailView/TrackDetailView.swift
@@ -19,8 +19,7 @@ class TrackDetailView: UIView {
     @IBOutlet weak var authorTitleLabel: UILabel!
     @IBOutlet weak var playPauseButton: UIButton!
     @IBOutlet weak var volumeSlider: UISlider!
-    
-    
+
     let player: AVPlayer = {
         let avplayer = AVPlayer()
         avplayer.automaticallyWaitsToMinimizeStalling = false
@@ -28,16 +27,23 @@ class TrackDetailView: UIView {
         
     }()
     
-    
+    // MARK: - awakeFromNib
     override func awakeFromNib() {
         super.awakeFromNib()
+
+        let scale: CGFloat = 0.8
+        trackImageView.transform = CGAffineTransform(scaleX: scale, y: scale)
+        trackImageView.layer.cornerRadius = 5
+
         
-        trackImageView.backgroundColor = .red
+//        trackImageView.backgroundColor = .red
     }
-    
+
+    // MARK: - Setup
     func set(viewModel: SearchViewModel.Cell) {
         trackTitleLabel.text = viewModel.trackName
         authorTitleLabel.text = viewModel.artistName
+        monitorStartTime()
         playTrack(previewUrl: viewModel.previewUrl)
         let string600 = viewModel.iconUrlString?.replacingOccurrences(of: "100x100", with: "600x600")
         guard let urlImage = URL(string: string600 ?? "") else { return }
@@ -52,8 +58,37 @@ class TrackDetailView: UIView {
         player.replaceCurrentItem(with: playerItem)
         player.play()
     }
-    
-    
+
+
+
+    private func monitorStartTime() {
+        let time = CMTimeMake(value: 1, timescale: 3)
+        let times = [NSValue(time: time)]
+        player.addBoundaryTimeObserver(forTimes: times, queue: .main) { [weak self] in
+            self?.enlargeTrackImageView()
+        }
+    }
+
+    deinit {
+        print("TrackDetail memory being reclaimed...")
+    }
+
+    // MARK: - Animation
+    private func enlargeTrackImageView() {
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: .curveEaseOut) {
+            self.trackImageView.transform = .identity
+        }
+
+    }
+
+    private func reduceTrackImageView() {
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: .curveEaseOut) {
+            let scale: CGFloat = 0.8
+            self.trackImageView.transform = CGAffineTransform(scaleX: scale, y: scale)
+        }
+    }
+
+    // MARK: - @IBAction
     @IBAction func handleCurrentTimerSlider(_ sender: Any) {
     }
     
@@ -75,13 +110,16 @@ class TrackDetailView: UIView {
         if player.timeControlStatus == .paused {
             player.play()
             playPauseButton.setImage(UIImage(imageLiteralResourceName: "pause"), for: .normal)
+            enlargeTrackImageView()
         } else {
             player.pause()
             playPauseButton.setImage(UIImage(imageLiteralResourceName: "play"), for: .normal)
+            reduceTrackImageView()
         }
         
         
     }
 }
+
 
 
